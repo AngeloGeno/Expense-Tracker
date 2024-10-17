c3d951251cb8d10603859320d4de5aa7459800b1
AngeloGeno <nhlanhlankomo466@gmail.com>
2024-10-17 23:26:42 +0200

    Transaction Controller Transaction UI

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -8,13 +8,29 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category ?Category { get; set; }
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string?  Note { get; set; }
 
         public DateTime Date { get; set; }  = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryWithIcon {
+            get{ 
+                return Category ==null? "" :Category.Icon + " " + Category.Title;
+            }
+                }
+
+        [NotMapped]
+        public string FormatedAmount
+        {
+            get
+            {
+                return (Category == null || Category.Type == "Expense") ? "- " + Amount.ToString("C0") : "+ " + Amount.ToString("C0");
+            }
+        }
 
     }
 }
